a089df87dd68a03c8b1794836a910ae44afdc872
ilyaly1103 <ilyalysenko1103@gmail.com>
2025-03-06 17:28:11 +0100

    Fixing

--- Desktop/8/vite.config.js
+++ Desktop/8/vite.config.js
@@ -5,10 +5,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    base: "/8-march/",
+    base: "/8-march/", 
   },
   build: {
     outDir: "dist",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
   },
-
 });
